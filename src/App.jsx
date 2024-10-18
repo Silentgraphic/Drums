9daffc38c1677abb7bde9e0261d70b9b4d5a0a50
silentgraphic <kierannolan1995@gmail.com>
2024-10-18 12:06:01 +0100

    Fixed CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Display from './display';
 function App() {
   return (
     <div id="drum-machine">
+      <h1>The Drum &#129345; Machine</h1>
       <DrumPad />
       <Display />
     </div>
